3f888a841f8bc5fce3fcc77163ae511cce6a5cd3
Vaibhav071799 <vaibhavdidmise1799sd@gmail.com>
2025-10-05 13:40:03 +0530

    chaging code

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ public class AddTwoNumbers {
         int number2 = 25;
 
         // Calculate the sum
-        int sum = number1 + number2;
+        int sum = number1 * number2;
 
         // Print the result
         System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
